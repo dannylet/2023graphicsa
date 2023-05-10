826b72878ec2583526921f35bb6d6cf77505a912
dannylet <092zxc6632@gmail.com>
2023-05-10 12:04:14 +0800

    week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,38 +1,81 @@
 #include<stdio.h>
 #include<GL/glut.h>
+#include "glm.h"
+GLMmodel * head = NULL;///week13 Step02-1
+GLMmodel * body = NULL;///week13 Step02-1
+GLMmodel * uparmL = NULL;///week13 Step02-1
+GLMmodel * lowarmL = NULL;///week13 Step02-1
+int show[4] = {0,1,0,0};
+void keyboard(unsigned char key, int x, int y)
+{
+    if(key== '0') show[0] = !show[0];///week13 Step03-1
+    if(key== '1') show[1] = !show[1];///week13 Step03-1
+    if(key== '2') show[2] = !show[2];///week13 Step03-1
+    if(key== '3') show[3] = !show[3];///week13 Step03-1
+    glutPostRedisplay();
+}
 FILE * fout =NULL;
 FILE * fin =NULL;
 float teapotX=0,teapotY=0;
+float angle =0,angle2=0,angle3=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glTranslatef(teapotX,teapotY, 0);
-        glutSolidTeapot(0.3);
+        glScalef(0.2,0.2,0.2);
+        ///glTranslatef(teapotX,teapotY, 0);
+        ///glutSolidTeapot(0.3);
+    if(body == NULL){
+        head = glmReadOBJ("model/head.obj");
+        body = glmReadOBJ("model/body.obj");///week13 Step02-1
+        uparmL = glmReadOBJ("model/uparmL.obj");///week13 Step03-1
+        lowarmL = glmReadOBJ("model/lowarmL.obj");///week13 Step03-1
+        ///glmUnitize(body);
+    }
+    if(show[0])glmDraw(head, GLM_MATERIAL);///week13 Step03-1
+    if(show[1])glmDraw(body, GLM_MATERIAL);///week13 Step03-1
+    glPushMatrix();
+        glTranslatef(teapotX,teapotY,0);
+        if(show[2])glmDraw(uparmL , GLM_MATERIAL);///week13 Step03-1
+    glPopMatrix();
+    if(show[3])glmDraw(lowarmL , GLM_MATERIAL);///week13 Step03-1
     glPopMatrix();
     glutSwapBuffers();
+}
+int oldX=0, oldY=0;
+void motion(int x,int y)
+{
+    teapotX += (x-oldX)/150.0;
+    teapotY -= (y-oldY)/150.0;
+    oldX = x;
+    oldY = y;
+    printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
+    glutPostRedisplay();
 }
 void mouse(int button, int state, int x,int y)
 {
     if(state==GLUT_DOWN)
     {
-        teapotX=(x-150)/150.0;
-        teapotY=(150-y)/150.0;
-        if(fout==NULL) fout = fopen ("file4.txt","w");
-        fprintf(fout,"%f %f\n", teapotX, teapotY);
+        oldX = x;
+        oldY = y;
+        angle = x;
+        ///teapotX=(x-150)/150.0;
+        ///teapotY=(150-y)/150.0;
+        ///if(fout==NULL) fout = fopen ("file4.txt","w");
+        ///fprintf(fout,"%f %f\n", teapotX, teapotY);
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-    if(fin==NULL)
-    {
-        fclose(fout);
-        fin = fopen("file4.txt", "r");
-    }
-    fscanf(fin, "%f %f", &teapotX,&teapotY);
-    display();
-}
+//void keyboard(unsigned char key, int x, int y)
+//{
+//    if(fin==NULL)
+//    {
+//        fclose(fout);
+//        fin = fopen("file4.txt", "r");
+//    }
+//    fscanf(fin, "%f %f", &teapotX,&teapotY);
+//    display();
+//}
 int main(int argc, char** argv)
 {
     glutInit(&argc,argv);
@@ -41,6 +84,7 @@ int main(int argc, char** argv)
 
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
+    glutMotionFunc(motion);
     glutKeyboardFunc(keyboard);
     glutMainLoop();
 }
