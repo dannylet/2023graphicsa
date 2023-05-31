8769c4de9ded4c9a7210e44173532c3e098d979e
dannylet <092zxc6632@gmail.com>
2023-06-01 00:27:52 +0800

    work1

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,9 +7,9 @@
 #include "glm.h"
 GLMmodel * head = NULL;///week13 Step02-1
 GLMmodel * body = NULL;///week13 Step02-1
-GLMmodel * uparmL = NULL;///week13 Step02-1
-GLMmodel * lowarmL = NULL;///week13 Step02-1
-int show[4] = {0,1,1,1};
+GLMmodel * righthand = NULL;///week13 Step02-1
+GLMmodel * lefthand = NULL;///week13 Step02-1
+int show[4] = {1,0,0,0};
 int ID=2;
 FILE * fout = NULL;
 FILE * fin = NULL;
@@ -54,34 +54,48 @@ void display()
     if(body == NULL){
         head = glmReadOBJ("model/head.obj");
         body = glmReadOBJ("model/body.obj");///week13 Step02-1
-        uparmL = glmReadOBJ("model/uparmL.obj");///week13 Step03-1
-        lowarmL = glmReadOBJ("model/lowarmL.obj");///week13 Step03-1
+        righthand = glmReadOBJ("model/righthand.obj");///week13 Step03-1
+        lefthand = glmReadOBJ("model/lefthand.obj");///week13 Step03-1
         ///glmUnitize(body);
     }
     if(ID==0) glColor3f(1,0,0);///選定的設紅色
-    else glColor3f(1,1,1);///沒選設白色
-    if(show[0])glmDraw(head, GLM_MATERIAL);///week13 Step03-1
+    else glColor3f(1,1,1);///沒選設白色///week13 Step03-1
+    glPushMatrix();
+        glTranslatef(-0.040000, 2.419998, 0);
+        glRotatef(angle[0] ,0,0,1);
+        ///glRotatef(angle[0], 1, 0, 0);///week15_step03-1
+        glTranslatef(0.040000, -2.419998, 0);
+        if(ID==0) glColor3f(1,0,0);///選定的設紅色
+        else glColor3f(1,1,1);///沒選設白色
+        if(show[0])glmDraw(head , GLM_MATERIAL);///week13 Step03-1
+    glPopMatrix();
+
+
 
     if(ID==1) glColor3f(1,0,0);///選定的設紅色
     else glColor3f(1,1,1);///沒選設白色
+
+
+
     if(show[1])glmDraw(body, GLM_MATERIAL);///week13 Step03-1
     glPushMatrix();
-        glTranslatef(0.786666, 0.140000, 0);
-        glRotatef(angle[2] ,0,0,1);///week15_step03-1
-        glTranslatef(-0.786666, -0.140000, 0);
+        glTranslatef(-1.299999, 1.733332, 0);
+        glRotatef(angle[2] ,0,0,1);
+        ///glRotatef(angle[2], 1, 0, 0);///week15_step03-1
+        glTranslatef(1.299999, -1.733332, 0);
         if(ID==2) glColor3f(1,0,0);///選定的設紅色
         else glColor3f(1,1,1);///沒選設白色
-        if(show[2])glmDraw(uparmL , GLM_MATERIAL);///week13 Step03-1
+        if(show[2])glmDraw(righthand , GLM_MATERIAL);///week13 Step03-1
     glPopMatrix();
 
 
     glPushMatrix();
-        glTranslatef(1.033333, -0.126667, 0);
+        glTranslatef(1.226666, 1.653332, 0);
         glRotatef(angle[3] ,0,0,1);///week15_step03-1
-        glTranslatef(-1.033333, 0.126667, 0);
+        glTranslatef(-1.226666, -1.653332, 0);
     if(ID==3) glColor3f(1,0,0);///選定的設紅色
     else glColor3f(1,1,1);///沒選設白色
-    if(show[3])glmDraw(lowarmL , GLM_MATERIAL);///week13 Step03-1
+    if(show[3])glmDraw(lefthand , GLM_MATERIAL);///week13 Step03-1
     glPopMatrix();
     glPopMatrix();
     glColor3f(0,1,0);
@@ -91,8 +105,8 @@ void display()
 int oldX=0, oldY=0;
 void motion(int x,int y)
 {
-    teapotX += (x-oldX)/150.0;
-    teapotY -= (y-oldY)/150.0;
+    teapotX += (x -oldX)/150.0;
+    teapotY -= (y -oldY)/150.0;
     angle[ID] += (x- oldX);///week15_step03-1
     oldX = x;
     oldY = y;
